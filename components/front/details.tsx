54f5fc6d4e30c528ef26e188a8f0f102c3b55aaf
Max Leiter <mleiter@usc.edu>
2020-08-27 17:48:22 -0700

    Modularize code

---
+++ components/front/details.tsx
@@ -0,0 +1,36 @@
+import Fade from 'react-reveal/Fade';
+import Image from '@/components/image';
+import FAQ from '@/components/front/faq';
+import styles from './details.module.css';
+
+const Details = () => (
+	<section className={styles.details} style={{ minHeight: '75vh' }}>
+		<Image className={styles.waves} height="375" path="waves.png" />
+		<h1 className={styles.detailsHeading}> Details </h1>
+		<div className={styles.detailsWrapper}>
+			<div style={{ flexDirection: 'column' }}>
+				<p>
+					<strong>Want to connect the world?</strong> This year we are focused
+					on ideas/solutions that will help connect our world during this time.
+					Get ready for a weekend full of innovation, connection, and education.
+					In an interdisciplinary endeavor, join together with 1000+ visionaries
+					to engage in workshops, listen to speakers, and develop programs,
+					tools, and relationships that can last for life. We hope to have you
+					join us!
+				</p>
+				<FAQ />
+			</div>
+		</div>
+		<Image alt="" className={styles.nodes} path={'nodes-2.png'} />
+		<div className={styles.hand}>
+			<Fade right>
+				<Image
+					alt="The world is in your hands at HackSC."
+					width="500"
+					path="handglobe.png"
+				/>
+			</Fade>
+		</div>
+	</section>
+);
+export { Details };
