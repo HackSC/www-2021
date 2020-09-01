372dc71b1fdccf91e4a613034ce41a5fa07280bd
Chloe Tanlimco <tanlimco@usc.edu>
2020-09-01 12:15:55 -0700

    reverted package.json

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -5,17 +5,15 @@ const CalendarSection = () => (
 	<>
 		<section className={styles.calendar}>
 			<div className={styles.calendarWrapper}>
-				<div style={{ flexDirection: 'column' }}>
-					<iframe
-						title="calendar"
-						src="https://calendar.google.com/calendar/embed?src=crcie5ocjmkf6uaju7q746seek%40group.calendar.google.com&ctz=America%2FLos_Angeles"
-						width="800"
-						height="600"
-						frameBorder="0"
-						scrolling="no"
-						style={{ height: 500 }}
-					></iframe>
-				</div>
+				<iframe
+					title="calendar"
+					src="https://calendar.google.com/calendar/embed?src=crcie5ocjmkf6uaju7q746seek%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+					width="800"
+					height="600"
+					frameBorder="0"
+					scrolling="no"
+					style={{ width: '75vw' }}
+				></iframe>
 			</div>
 		</section>
 	</>
