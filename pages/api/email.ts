3e59be81d49b6b4b7d3d7a961a9735b34f4113de
Andreas Bigger <abigger87@gmail.com>
2020-09-22 21:37:05 -0700

    fix serverless mail subscribe function

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -11,8 +11,8 @@ const query = async (req, res) => {
 
 	const subs = await db.query(escape`
         SELECT *
-        FROM ${process.env.PROD_DB_NAME}
-        WHERE email = '${email}'
+        FROM signups
+        WHERE email=${email}
     `);
 
 	if (subs.length > 0)
@@ -21,11 +21,19 @@ const query = async (req, res) => {
 			.json({ success: null, error: 'Email already exists in the database.' });
 
 	const result = await db.query(escape`
-        INSERT INTO ${process.env.PROD_DB_NAME}
-        VALUES ('${email}', '${my_ip}', '${Date.now()}')
+        INSERT INTO signups (email, ip, created_at)
+        VALUES (${email}, ${my_ip}, ${toSqlDatetime(new Date())})
     `);
 
 	res.status(200).json({ success: email, error: null });
+};
+
+const toSqlDatetime = (inputDate) => {
+	const date = new Date(inputDate);
+	const dateWithOffest = new Date(
+		date.getTime() - date.getTimezoneOffset() * 60000
+	);
+	return dateWithOffest.toISOString().slice(0, 19).replace('T', ' ');
 };
 
 export default query;
