f7f7b72b5b10b201a9f08f30cc570ec00cb4c2da
Max Leiter <mleiter@usc.edu>
2020-10-08 17:25:57 -0700

    remove animation from connect temporarily

--- pages/hacksconnect.tsx
+++ pages/hacksconnect.tsx
@@ -5,7 +5,6 @@ import Header from '@/components/header';
 import Footer from '@/components/front/footer';
 import { NextPage } from 'next';
 import Cards from '@/components/connect/cards';
-import { Fade } from 'react-reveal/Fade';
 
 const PDF = dynamic(() => import('@/components/connect/pdf'), { ssr: false });
 
@@ -48,58 +47,53 @@ const HackSConnect: NextPage = () => {
 				<link rel="icon" href="/images/favicon_64.png" />
 			</Head>
 			<div style={{ marginBottom: 'calc(var(--gap-double) * 2)' }}>
-				<Fade>
-					<Header />
-				</Fade>
+				<Header />
 				<section className="first">
-					<Fade ssrFadeout>
-						<h1> HackSConnect </h1>
-						<p>
-							Connect with other creators to <span>grow your ideas</span> and{' '}
-							<span> flourish.</span>{' '}
-						</p>
-						<p>10.08.20 | 6 - 9 PM PST</p>
-						<p className="desc">
-							HackSConnect is a virtually hosted idea exchange platform for
-							collaboration between companies, teams, and ideas. Open to all
-							majors, all are welcome to connect.{' '}
-						</p>
-						<p className="desc">
-							Expanding your network virtually can be hard but HackSConnect will
-							be a garden of students buzzing, connecting and exchanging rad
-							ideas and will help you find great new recruits, ideas, and
-							co-founders!
-						</p>
-						<p className="desc">
-							Attendees will be divided into the following three groups:{' '}
-						</p>
-						<Cards />
-						<p className="desc">
-							Our Seeds and Sprouts registration is closed, but Bee attendees
-							are still encouraged to sign-up{' '}
-							<a href="https://hacksc.typeform.com/to/hfbb6YDj">here!</a>
-						</p>
-						<h2>Event Schedule (PST)</h2>
-						<ul
-							style={{
-								width: '70%',
-								margin: 'var(--gap) auto',
-								textAlign: 'left',
-							}}
-						>
-							<li>6:00 &mdash; 6:15 pm: Event lobby and Introduction</li>
-							<li>
-								6:15 pm &mdash; 7:00 pm: Fireside Chat with CEO of Highkey, Vili
-								Vaanenan
-							</li>
-							<li>
-								7:00 pm &mdash; 9:00 pm: Open Expo: Bees can buzz around and
-								meet our seeds and sprouts!
-							</li>
-						</ul>
-						<h2>Important Information</h2>
-						<PDF />
-					</Fade>
+					<h1> HackSConnect </h1>
+					<p>
+						Connect with other creators to <span>grow your ideas</span> and{' '}
+						<span> flourish.</span>{' '}
+					</p>
+					<p>10.08.20 | 6 - 9 PM PST</p>
+					<p className="desc">
+						HackSConnect is a virtually hosted idea exchange platform for
+						collaboration between companies, teams, and ideas. Open to all
+						majors, all are welcome to connect.{' '}
+					</p>
+					<p className="desc">
+						Expanding your network virtually can be hard but HackSConnect will
+						be a garden of students buzzing, connecting and exchanging rad ideas
+						and will help you find great new recruits, ideas, and co-founders!
+					</p>
+					<p className="desc">
+						Attendees will be divided into the following three groups:{' '}
+					</p>
+					<Cards />
+					<p className="desc">
+						Our Seeds and Sprouts registration is closed, but Bee attendees are
+						still encouraged to sign-up{' '}
+						<a href="https://hacksc.typeform.com/to/hfbb6YDj">here!</a>
+					</p>
+					<h2>Event Schedule (PST)</h2>
+					<ul
+						style={{
+							width: '70%',
+							margin: 'var(--gap) auto',
+							textAlign: 'left',
+						}}
+					>
+						<li>6:00 &mdash; 6:15 pm: Event lobby and Introduction</li>
+						<li>
+							6:15 pm &mdash; 7:00 pm: Fireside Chat with CEO of Highkey, Vili
+							Vaanenan
+						</li>
+						<li>
+							7:00 pm &mdash; 9:00 pm: Open Expo: Bees can buzz around and meet
+							our seeds and sprouts!
+						</li>
+					</ul>
+					<h2>Important Information</h2>
+					<PDF />
 				</section>
 				<style jsx global>
 					{`
@@ -138,9 +132,7 @@ const HackSConnect: NextPage = () => {
 					`}
 				</style>
 			</div>
-			<Fade>
-				<Footer />
-			</Fade>
+			<Footer />
 		</>
 	);
 };
