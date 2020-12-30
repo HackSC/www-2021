334ad0e9ede24ae4095fbcd91b7d0d4d5127a67d
Elissa Perdue <elissaperdue@gmail.com>
2020-12-29 22:46:13 -0500

    verticals details added

--- components/front/connectivity.tsx
+++ components/front/connectivity.tsx
@@ -3,12 +3,21 @@ import Verticals from '@/components/front/verticals';
 import styles from './connectivity.module.css';
 
 const Connectivity = () => (
-	<section style={{ height: 'inherit', minHeight: '80vh', marginTop: 100 }}>
-		<div className={styles.connectivity}>
-			<div className={styles.heading}>
-				<h2> 2021 Verticals </h2>
+	<section className={styles.connectivity} id="details">
+		<h2 className={styles.connectivityHeading}> Verticals </h2>
+		<div className={styles.connectivityWrapper}>
+			<div style={{ flexDirection: 'column' }}>
+				<p>
+					In order to address this tumultuous time of COVID-19 and social
+					distancing, HackSC 2021 is all about <strong>Connectivity</strong>. At
+					HackSC, hackers can create technology solutions to bring the world
+					closer together under four verticals: device, team, customer, and
+					person
+				</p>
 			</div>
-			<h4>Connectivity</h4>
+		</div>
+
+		<div>
 			<Verticals />
 			<Image alt="" className={styles.nodes} path={'nodes-2.png'} />
 		</div>
