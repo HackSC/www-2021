19cf3b7e49404c5fa856a28b6635e08f7c8be847
Elissa Perdue <elissaperdue@gmail.com>
2021-01-16 15:08:39 -0500

    blurb

--- components/front/prizes.tsx
+++ components/front/prizes.tsx
@@ -6,8 +6,15 @@ const Prizes = () => (
 	<section className={styles.prizes} id="details">
 		<h2 className={styles.prizesHeading}> Prizes </h2>
 		<div>
-			At HackSC 2021, HackSC Vertical winners will get to select one of the
-			following prize packages
+			<div className={styles.prizesWrapper}>
+				<div style={{ flexDirection: 'column' }}>
+					<p>
+						At HackSC 2021, HackSC Vertical winners will get to select from one
+						of our five curated prize packages.
+					</p>
+				</div>
+			</div>
+
 			<Packages />
 		</div>
 	</section>
