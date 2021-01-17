cf69f1735573247fa44d1cbff147d69975697e74
Elissa Perdue <elissaperdue@gmail.com>
2021-01-16 23:00:10 -0500

    alt descriptions

--- components/front/prizePackages.tsx
+++ components/front/prizePackages.tsx
@@ -3,28 +3,37 @@ import Fade from 'react-reveal/Fade';
 interface Prize {
 	name: string;
 	color: string;
+	description: string;
 }
 
 const PrizeInfo: Prize[] = [
 	{
 		name: 'outdoors',
 		color: '#A4D5B2',
+		description:
+			' The Great Outdoors Package - electronic skateboard and hydroflask',
 	},
 	{
 		name: 'lazyOutdoors',
 		color: '#7968AE',
+		description:
+			'The Lazy Great Outdoors Package - picnic basket, picnic blanket, and outdoor speaker',
 	},
 	{
 		name: 'tunes',
 		color: '#D70E65',
+		description: 'The Fancy Tunes Package - record player and records/speaker',
 	},
 	{
 		name: 'netflix',
 		color: '#AA2208',
+		description:
+			'The Netflix and Code Package - projector and amazon firestick',
 	},
 	{
 		name: 'beach',
 		color: '#FFD84C',
+		description: 'The Basic Beach Package - $120 Amazon giftcard',
 	},
 ];
 const Packages = () => {
@@ -32,12 +41,12 @@ const Packages = () => {
 		<div className={styles.row}>
 			<div className={styles.prizes}>
 				<Fade>
-					{PrizeInfo.map(({ name, color }) => {
+					{PrizeInfo.map(({ name, color, description }) => {
 						return (
 							<div className={styles.column} key={name}>
 								<div className={styles.card}>
 									&nbsp;
-									<img alt="" src={`images/prizes/${name}.png`} />
+									<img alt={description} src={`images/prizes/${name}.png`} />
 								</div>
 							</div>
 						);
