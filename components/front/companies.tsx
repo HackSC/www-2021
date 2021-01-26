82730c5d418d59002a892e127d61b717941a8e39
Elissa Perdue <elissaperdue@gmail.com>
2021-01-26 09:52:06 -0800

    new sponsors

--- components/front/companies.tsx
+++ components/front/companies.tsx
@@ -6,13 +6,13 @@ interface Company {
 }
 
 const GoldCompanyInfo: Company[] = [
-	// {
-	// 	name: 'rippleX',
-	// 	url: 'https://ripplex.io/',
-	// },
 	{
 		name: 'intersystems',
 		url: 'https://www.intersystems.com/',
+	},
+	{
+		name: 'rippleX',
+		url: 'https://ripplex.io/',
 	},
 	{
 		name: 'appsmith',
@@ -44,8 +44,8 @@ const BronzeCompanyInfo: Company[] = [
 
 const InKindInfo: Company[] = [
 	{
-		name: 'stickermule',
-		url: 'https://www.stickermule.com/',
+		name: 'GoogleCloud',
+		url: 'https://cloud.google.com/',
 	},
 	{
 		name: 'vercel',
@@ -55,10 +55,10 @@ const InKindInfo: Company[] = [
 		name: 'DigitalOcean',
 		url: 'https://www.digitalocean.com/',
 	},
-	// {
-	// 	name: 'gcp',
-	// 	url: 'https://www.digitalocean.com/',
-	// },
+	{
+		name: 'stickermule',
+		url: 'https://www.stickermule.com/',
+	},
 ];
 
 const GoldCompanies = () => {
