b5726090b293a0ace322a6394b95dd5f1482e859
Max Leiter <maxwell.leiter@gmail.com>
2021-08-31 12:26:39 -0700

    application redirect

--- next.config.js
+++ next.config.js
@@ -4,14 +4,29 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 const imagemin = require('imagemin');
 const imageminOptipng = require('imagemin-optipng');
- 
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-    optimizeImagesInDev: false,
-    imageTrace: {
-      skipTraceIfBase64: true
-    }
-  }],
-]);
 
+module.exports = withPlugins(
+	[
+		[
+			optimizedImages,
+			{
+				/* config for next-optimized-images */
+				optimizeImagesInDev: false,
+				imageTrace: {
+					skipTraceIfBase64: true,
+				},
+			},
+		],
+	],
+	{
+		async redirects() {
+			return [
+				{
+					source: '/apply',
+					destination: 'https://hacksc.typeform.com/organizerapp',
+					permanent: false,
+				},
+			];
+		},
+	}
+);
